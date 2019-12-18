b54409a22a0539fe8c8a058ef9cad3d24c6bf5a1
syahdanny <syahdanny11@gmail.com>
2019-12-18 20:59:57 +0700

    menambahkan settings

---
+++ app/src/main/java/com/example/gaslur/ui/settings/SettingsFragment.java
@@ -0,0 +1,25 @@
+package com.example.gaslur.ui.settings;
+
+import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.TextView;
+
+import androidx.annotation.Nullable;
+import androidx.annotation.NonNull;
+import androidx.fragment.app.Fragment;
+import androidx.lifecycle.Observer;
+import androidx.lifecycle.ViewModelProviders;
+import androidx.preference.PreferenceFragmentCompat;
+
+import com.example.gaslur.R;
+
+public class SettingsFragment extends PreferenceFragmentCompat {
+
+    @Override
+    public void onCreatePreferences(Bundle savedInstanceState, String rootKey) {
+        setPreferencesFromResource(R.xml.preferences, rootKey);
+    }
+
+}
